a3eadef6a7af8f4a0b34a04118b0d4553fdc413a
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-11-27 16:26:35 -0600

    Created a Nav component with Links and sourced it into app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,18 @@ import axios from 'axios';
 import './App.css';
 import StudentForm from '../StudentForm/StudentForm.jsx';
 import StudentList from '../StudentList/StudentList.jsx';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Nav from './Nav.jsx';
 
 function App() {
     return (
         <div className="App">
+            <Router>
             <header className="App-header">
-            <h1 className="App-title">GitHub Student List</h1>
+                <h1 className="App-title">GitHub Student List</h1>
             </header>
+            <Nav/>
+            </Router>
             <br/>
             <StudentForm />
 
